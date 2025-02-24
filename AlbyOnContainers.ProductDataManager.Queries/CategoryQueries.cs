7385fb125316686a7d5fc1fe353997df752e8e97
Alberto Viezzi <alberto.viezzi@euris.it>
2025-02-24 22:20:00 +0100

    implement category management: add expand/collapse functionality and improve search handling in Categories page

--- AlbyOnContainers.ProductDataManager.Queries/CategoryQueries.cs
+++ AlbyOnContainers.ProductDataManager.Queries/CategoryQueries.cs
@@ -5,31 +5,59 @@ namespace AlbyOnContainers.ProductDataManager.Queries;
 
 public class CategoryQueries(ICategoryRepository repository)
 {
-    public Task<List<TreeItemData<Category>>> GetAllParents() => 
+    public IAsyncEnumerable<TreeItemData<Category>> GetAllParents() =>
         GetAll(() => repository.GetAllChildren());
-    
-    public Task<List<TreeItemData<Category>>> GetAllChildren(Guid parentId) => 
+
+    public IAsyncEnumerable<TreeItemData<Category>> GetAllChildren(Guid parentId) =>
         GetAll(() => repository.GetAllChildren(parentId));
 
     public async Task<List<(Guid Id, string Name)>> GetAllCategoriesNames()
     {
         var categories = await repository.GetAll()
-                             .Select(category => (category.Id, Name: category.Name))
+                             .Select(category => (category.Id, category.Name))
                              .OrderBy(category => category.Name)
                              .ToListAsync();
 
         categories.Insert(0, (Guid.Empty, "Root"));
-        
+
         return categories;
     }
 
+    public async IAsyncEnumerable<TreeItemData<Category>> GetAllHierarchy()
+    {
+        var categories = await repository.GetAll().OrderBy(c => c.ParentId != null).ToListAsync();
+        var lookup = categories.ToLookup(c => c.ParentId);
     
-    static async Task<List<TreeItemData<Category>>> GetAll(Func<IAsyncEnumerable<Category>> selector) =>
-        await selector()
+        async IAsyncEnumerable<TreeItemData<Category>> BuildTree(Guid? parentId)
+        {
+            foreach (var category in lookup[parentId].OrderBy(c => c.Name))
+            {
+                var children = await BuildTree(category.Id).ToListAsync();
+                var item = new TreeItemData<Category>
+                {
+                    Text = category.Name,
+                    Value = category,
+                    Expandable = children.Any(),
+                    Children = children.ToList()
+                };
+                yield return item;
+            }
+        }
+    
+        await foreach (var item in BuildTree(null))
+        {
+            yield return item;
+        }
+    }
+
+
+
+    static IAsyncEnumerable<TreeItemData<Category>> GetAll(Func<IAsyncEnumerable<Category>> selector) =>
+        selector()
             .Select(category => new TreeItemData<Category>
             {
                 Text = category.Name,
                 Value = category,
                 Expandable = category.HasChildren
-            }).ToListAsync();
+            }).AsAsyncEnumerable();
 }
